405e602e8e2f91c6e255a0c5b760b16a3ce4417b
Finasopretty <iyamuosasere962@gmail.com>
2026-07-06 10:49:49 -0500

    Refactor HelloFunction to use HttpResponseData

--- HelloFunction.cs
+++ HelloFunction.cs
@@ -1,7 +1,7 @@
+using System.Net;
 using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Http;
 using Microsoft.Extensions.Logging;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
 
 namespace azure_functions_lab;
 
@@ -15,9 +15,14 @@ public class HelloFunction
     }
 
     [Function("HelloFunction")]
-    public IActionResult Run([HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequest req)
+    public HttpResponseData Run(
+        [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequestData req)
     {
         _logger.LogInformation("C# HTTP trigger function processed a request.");
-        return new OkObjectResult("Welcome to Azure Functions!");
+
+        var response = req.CreateResponse(HttpStatusCode.OK);
+        response.WriteString("Welcome to Azure Functions!");
+
+        return response;
     }
 }
